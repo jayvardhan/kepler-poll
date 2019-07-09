18086b384ad6eb803695cd3642cb67fda02db77c
root <vardhan.jay@gmail.com>
2019-07-09 12:33:06 +0530

    removed css classes from fep form

--- admin/templates/fep-form.php
+++ admin/templates/fep-form.php
@@ -1,17 +1,15 @@
-<div class='row kepler-poll-form' data-url='<?php _e($url); ?>' data-token='<?php _e($token); ?>'>
-	<div class='col-sm-12'>
-		<div class='add-poll-result'></div>
-		<input type='text' class='form-control' id='add-poll-question' placeholder='Question'>
-		
-		<br/>
-		
-		<div class='poll-choice-wrapper'>
-			<input type='text' class='form-control add-poll-choice'   placeholder='Choice'>
-			<input type='text' class='form-control add-poll-choice'  placeholder='Choice'>
-		</div>
+<div class='kepler-poll-form' data-url='<?php _e($url); ?>' data-token='<?php _e($token); ?>'>
+	<div class='add-poll-result'></div>
+	<input type='text' class='form-control' id='add-poll-question' placeholder='Question'>
+	
+	<br/>
+	
+	<div class='poll-choice-wrapper'>
+		<input type='text' class='form-control add-poll-choice'  placeholder='Choice'>
+		<input type='text' class='form-control add-poll-choice'  placeholder='Choice'>
+	</div>
 
-		<button  class='btn pull-right' id='add-choice-btn'>+ Add Choice</button>
+	<button  class='btn pull-right' id='add-choice-btn'>+ Add Choice</button>
 
-		<button type='submit' class='btn btn-primary' id='kepler-create-poll'>Create Poll &nbsp; <i class='fa fa-refresh fa-spin' style='display: none;'></i></button>	
-	</div>		
+	<button type='submit' class='btn btn-primary' id='kepler-create-poll'>Create Poll &nbsp; <i class='fa fa-refresh fa-spin' style='display: none;'></i></button>	
 </div>
